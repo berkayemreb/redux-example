56343754607c21ec8906650c01925d9101cfceaf
bbogum19 <berkaye2001@hotmail.com>
2023-02-14 10:43:50 +0300

    Created TextInput component.

--- src/pages/Primary.js
+++ src/pages/Primary.js
@@ -1,12 +1,24 @@
-import React from "react";
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from 'react-native';
 
 const Primary = () => {
+
+    const [text, setText] = useState("");
+
+    const handleAdd = () => {
+        setText("")
+        console.log(text);
+    }
     return (
         <View>
-            <Text>
-                Primary Page!
-            </Text>
+            <View style={{ borderWidth: 1, margin: 10, padding: 5 }}>
+                <TextInput
+                    value={text}
+                    onChangeText={setText}
+                    placeholder="İsim Giriniz..."
+                />
+            </View>
+            <Button title="Ekle" onPress={handleAdd} />
         </View>
     )
 }
